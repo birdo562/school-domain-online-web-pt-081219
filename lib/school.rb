1328224ff1ba2494db5c3e72ddbf9ab46efbd023
Roberto Medina <robby.medina562@gmail.com>
2019-09-04 02:19:17 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,7 @@ class School
   def add_student (name, grade)
     @student_name= name
     @grade = grade
-    if @roster.include? == false
+    if @roster.include?(grade) == false
       @roster[grade] = []
 end
 @roster[grade] << name
